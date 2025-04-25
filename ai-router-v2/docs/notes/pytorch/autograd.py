4a7497177aebce78cdb01f004ee0981ffde4e6e7
zfan2356 <2356754953@qq.com>
2025-04-26 00:24:29 +0800

    add autograd

--- ai-router-v2/docs/notes/pytorch/autograd.py
+++ ai-router-v2/docs/notes/pytorch/autograd.py
@@ -8,3 +8,24 @@ class MyRelu(torch.nn.Module):
      
      def forward(self, input: torch.Tensor):
           return F.relu(input)
+
+
+class _MyExp(torch.autograd.Function):
+     @staticmethod
+     def forward(ctx, input: torch.Tensor):
+          output = input.exp()
+          ctx.save_for_backward(output)
+          return output
+
+     @staticmethod
+     def backward(ctx, grad_output: torch.Tensor):
+          output, = ctx.saved_tensors
+          return grad_output * output
+
+def my_exp(input: torch.Tensor):
+     return _MyExp.apply(input)
+
+
+
+
+          
